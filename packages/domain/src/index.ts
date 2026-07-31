29b992d95d70013fd41a2825ebabd74d9b4115ef
zoumana007 <zoumanacamara876@gmail.com>
2026-07-31 07:17:48 +0200

    feat(domain): exporter le calcul des soldes

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./fee-policy.js";
 export * from "./ledger-account.js";
+export * from "./ledger-balance.js";
 export * from "./ledger.js";
 export * from "./money.js";
 export * from "./rate.js";
