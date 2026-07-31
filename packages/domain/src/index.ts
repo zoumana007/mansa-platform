0afe4431a4fe216d1dcd2d2ccbcc6379b830eabb
zoumana007 <zoumanacamara876@gmail.com>
2026-07-31 08:17:12 +0200

    feat(domain): exporter l’idempotence

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./available-balance.js";
 export * from "./fee-policy.js";
+export * from "./idempotency.js";
 export * from "./ledger-account.js";
 export * from "./ledger-balance.js";
 export * from "./ledger.js";
