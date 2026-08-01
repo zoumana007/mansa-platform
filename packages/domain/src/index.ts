4679d1ae76f1781c22abddf8101e45a3d40271c9
zoumana007 <zoumanacamara876@gmail.com>
2026-08-01 13:42:19 +0200

    feat(domain): exporter les affectations employés-points de vente

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -9,6 +9,7 @@ export * from "./mansa-handle-directory.js";
 export * from "./mansa-handle.js";
 export * from "./merchant-location.js";
 export * from "./merchant-profile.js";
+export * from "./merchant-staff-assignment.js";
 export * from "./merchant-staff-member.js";
 export * from "./money.js";
 export * from "./payment-card.js";
