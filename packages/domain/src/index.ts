9b015d2fdabbb223d646b5876444d7ceec28699c
zoumana007 <zoumanacamara876@gmail.com>
2026-08-01 10:37:39 +0200

    feat(domain): exporter le profil commerçant

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -7,6 +7,7 @@ export * from "./ledger-balance.js";
 export * from "./ledger.js";
 export * from "./mansa-handle-directory.js";
 export * from "./mansa-handle.js";
+export * from "./merchant-profile.js";
 export * from "./money.js";
 export * from "./payment-card.js";
 export * from "./qr-payment-intent.js";
